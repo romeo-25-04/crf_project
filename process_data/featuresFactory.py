a3316f0e2063cba7094deb2f0c8432e89771d9f3
Roman-Admin <rcapsamun@yahoo.com>
2017-04-03 12:42:48 +0200

    add word shapes feature

--- process_data/featuresFactory.py
+++ process_data/featuresFactory.py
@@ -1,11 +1,15 @@
+import re
+
+
 class SentenceFeaturesFactory:
 
     GAZET_NAMES = ['Roman', 'Jue']
 
     def __init__(self, sent):
         self.sent = sent
-        self.features = self.word_features()
+        self.features = self.word_features
 
+    @property
     def word_features(self):
         word_feat = []
         for i, word in enumerate(self.sent):
@@ -33,7 +37,42 @@ class SentenceFeaturesFactory:
                 'word+1': next_word,
                 'word+1:isCapitalized': next_word[0].isupper(),
                 'word+1:suff4': next_word[-4:],
-                'word+1:isPunct': next_word in ',.!?'
+                'word+1:isPunct': next_word in ',.!?',
+                'wordShape': word_shape(word)
             }
             word_feat.append(word_dict)
         return word_feat
+
+
+def word_shape(word):
+    """
+    Encode words attributes
+        Upper cased letter     --> X
+        lower cased letter     --> x
+        Number                 --> d
+        Punctuation            --> to itself
+        Length                 --> if len < 5: encode all letter
+                                   elif no (numbers or punct) in word: encode 2 first and 2 last letters
+                                   else:
+    """
+    encoded = ''
+    for ch in word:
+        if ch.isupper():
+            encoded += 'X'
+        elif ch.islower():
+            encoded += 'x'
+        elif ch.isdigit():
+            encoded += 'd'
+        else:
+            encoded += ch
+    if len(encoded) > 4:
+        first = encoded[:2]
+        last = encoded[-2:]
+        center = encoded[2:-2]
+        center = re.sub(r"x+", r"x", center)
+        center = re.sub(r"(x\.)+", r"x.", center)
+        center = re.sub(r"X+", r"X", center)
+        center = re.sub(r"d+", r"d", center)
+        encoded = first + center + last
+
+    return encoded
