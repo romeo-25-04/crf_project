f8bfc4062f8cec2f4a6f386b245dde29f02a4959
Roman-Admin <rcapsamun@yahoo.com>
2018-03-21 17:00:23 +0100

    add demo info and data source in view

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, request
+from flask import render_template, request, redirect
 
 from process_data.sentence import Sentence
 from tagger.tagger_str import tag_sent
@@ -7,14 +7,14 @@ from tagger.tagger_str import tag_sent
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    sent_str=''
+    sent_str = ''
     sentence = Sentence('demo_app', [])
     if request.method == "POST":
         result = request.form
         sent_str = result.get('exampleSentence', '')
         sentence = tag_sent(sent_str)
     return render_template('index.html',
-                           title="NER-Tagging in german",
+                           title="Demo for NER-Tagging in german",
                            sent_str=sent_str,
                            sentence=[(word.token, word.outer_label_pred)
                                      for word in sentence.sent])
